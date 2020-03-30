d71e8010e09872d349758ad7f0326429785bed96
Rouse <zrouse@ford.com>
2020-03-30 16:46:04 -0400

    Added proper logic for exporting filenames and filetypes

--- reg_parse.py
+++ reg_parse.py
@@ -1,13 +1,37 @@
-import yarp
-import xls_writer
+# Python Project 2
+# Zack Rouse
+# CSI 5740
+# A python script to parse recent document data from registry hives.
+
+# native
+import logging
 import argparse
+
+# 3rd party
+import xlsxwriter
 from yarp import Registry
 
 reg_file = open('NTUSER.DAT', 'rb')
 reg = Registry.RegistryHive(reg_file)
+recent_docs = reg.find_key('SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\RecentDocs')
 
-docs = reg.find_key('SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\RecentDocs')
+def export_filenames(recent_docs):
+    for i, value in enumerate(recent_docs.subkeys()):
+        #print(f"{i}) {value.name()}, {value.values_count()}")
+        print(value.name())
+        k = recent_docs.subkey(value.name())
+        for j, thing in enumerate(k.values()):
+            try:
+                if thing.name() != "MRUListEx":
+                # filename will provide a parsed string of the filename from REG_BINARY raw data output from yarp. Without this, we have ugly raw data.
+                    filename = thing.data()[::2][:thing.data()[::2].find(b'\x00')].decode()
+                    print(f"    {filename}")
+            except:
+                print(f"No data for: {thing.name()}")
 
+def export_filetypes(recent_docs):
+    for i, value in enumerate(recent_docs.subkeys()):
+        print(f"{i}) {value.name()}, {value.values_count()}")
 
 def main():
     # arg parser
@@ -18,14 +42,7 @@ def main():
     # parse the arguments
     args = parser.parse_args()
 
-
-
-
-
-    print(docs.last_written_timestamp())
-
-    for i, value in enumerate(docs.subkeys()):
-        print(f"{i}) {value.name()}, {value.values_count()}")
+    export_filenames(recent_docs)
 
 
 if __name__ == "__main__":
